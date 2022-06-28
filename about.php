a42e52ac836c551c83279a7fac018461e7df5b7e
AnisNizam <108330436+AnisNizam@users.noreply.github.com>
2022-06-28 21:03:33 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>complete responsive coffee shop website design</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+
+    <nav class="navbar">
+        <a href="index.php">home</a>
+        <a href="about.php">about</a>
+        <a href="menu.php">menu</a>
+        <a href="products.php">products</a>
+	<a href="review.php">review</a>
+   </nav>
+
+    <div class="icons">
+        
+        <div class="fas fa-shopping-cart" id="cart-btn"></div>
+        <div class="fas fa-bars" id="menu-btn"></div>
+    </div>
+
+
+    <div class="cart-items-container">
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/1.JPG" alt="">
+            <div class="content">
+                <h3>Disney Princess Junior Hoodie Backpack</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/2.JPG" alt="">
+            <div class="content">
+                <h3>Marvel Trolley Backpack With Light Up Wheels</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/3.JPG" alt="">
+            <div class="content">
+                <h3>Disney Princess Ariel Weekender Bag</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/4.JPG" alt="">
+            <div class="content">
+                <h3>Disney Princess Stainless Steel Spritz Drink Bottle 500Ml</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- header section ends -->
+
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="images/about-img.jpeg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>OUR HISTORY</h3>
+            <p>Smiggle is the ultimate destination for your school, birthday and leisure essentials. From backpacks and drink bottles, to gift packs and keyrings, Smiggle is the original creator of all things colourful, fun, unique and on trend.
+
+                Smiggle was established in 2003 in Melbourne Australia, originally opening 20 concept stores around the country. By 2007, Smiggle had joined the Just Group – owned by Premier Investments Limited, the ASX listed retail conglomerate.
+
+                Fast-forward to 2016 and Smiggle stores had rapidly expanded across Australia and could also be found in the United Kingdom, Ireland, New Zealand, Singapore and Malaysia.
+
+                Today, Smiggle retails from hundreds of standalone stores and wholesale "shop-in-shop" concepts, across multiple countries.</p>
+
+            <a href="#" class="btn">learn more</a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-pinterest"></a>
+    </div>
+
+    <div class="links">
+        <a href="index.php">home</a>
+        <a href="about.php">about</a>
+        <a href="menu.php">menu</a>
+        <a href="products.php">products</a>
+	<a href="review.php">review</a>
+    </div>
+
+    <div class="credit">created by <span>mr. web designer</span> | all rights reserved</div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
